a4d3f700fa805a8c237e9245b9189137fdeb7436
Duong Nguyen <duong.nguyen@esgtech.co>
2023-06-18 21:20:21 +0700

    Change BE api

--- src/API/axiosClient.js
+++ src/API/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://thanh-server.herokuapp.com',
+    baseURL: 'https://ecommerce-json-server.vercel.app',
     headers: {
         'Content-Type': 'application/json',
     },
