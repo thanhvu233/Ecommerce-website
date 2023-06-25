c1cf8186a590b4d999836984d1d08ac2aa51546c
Duong Nguyen <duong.nguyen@esgtech.co>
2023-06-25 16:13:37 +0700

    Refactor returned error message from axios

--- src/API/axiosClient.js
+++ src/API/axiosClient.js
@@ -19,7 +19,7 @@ axiosClient.interceptors.response.use(
         return { data: response.data.data };
     },
     function (error) {
-        return Promise.reject(error);
+        return Promise.reject(error.response.data);
     }
 );
 
