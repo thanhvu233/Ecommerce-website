f384e69584eea0dbae9a9a0195f5bc0ce6508ca0
Duong Nguyen <duong.nguyen@esgtech.co>
2023-07-03 23:40:18 +0700

    Change axios base url

--- src/API/axiosClient.js
+++ src/API/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://127.0.0.1:3001/api/v1',
+  baseURL: 'https://ecommerce-website-nodejs.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
