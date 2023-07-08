c63e816c65d504be843218e72a5a6d48eb60b466
Duong Nguyen <duong.nguyen@esgtech.co>
2023-07-08 13:29:50 +0700

    Refactor private route

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useHistory } from 'react-router-dom';
+import userApi from '../../API/userApi';
 
 function PrivateRoute(props) {
   // check localStorage and find property access_token
   // If found: render <Route> tag
   // If not found: redirect to Login page
 
-  const isLogin = localStorage.getItem('access_token');
+  const history = useHistory();
 
-  return <>{isLogin ? <Route {...props} /> : <Redirect to='/login' />}</>;
+  const checkLogin = async () => {
+    try {
+      const { data } = await userApi.getCurrentUser();
+
+      if (data) {
+        return true;
+      }
+
+      return false;
+    } catch (error) {
+      localStorage.clear();
+      localStorage.setItem('path', props.path);
+
+      history.push('/login');
+
+      return false;
+    }
+  };
+
+  return <>{checkLogin() ? <Route {...props} /> : <Redirect to='/login' />}</>;
 }
 
 export default PrivateRoute;
